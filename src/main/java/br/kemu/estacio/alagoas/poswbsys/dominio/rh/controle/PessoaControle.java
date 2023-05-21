86a63cc52f5e50bb81b644113f8e13cff92049f5
Kemuel Kesley <kemuelkesleytkd@outlook.com>
2023-05-20 21:38:13 -0300

    Foi adicionado a tela de adicionar cliente e a tela nova que inclui um cadastrato

--- src/main/java/br/kemu/estacio/alagoas/poswbsys/dominio/rh/controle/PessoaControle.java
+++ src/main/java/br/kemu/estacio/alagoas/poswbsys/dominio/rh/controle/PessoaControle.java
@@ -3,7 +3,10 @@ package br.kemu.estacio.alagoas.poswbsys.dominio.rh.controle;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import br.kemu.estacio.alagoas.poswbsys.dominio.rh.Pessoa;
 import br.kemu.estacio.alagoas.poswbsys.dominio.rh.PessoaRepositorio;
 
 @Controller
@@ -20,6 +23,19 @@ public class PessoaControle {
 	public String pessoas(Model model) {
 		model.addAttribute("listaPessoas", pessoaRepo.findAll());
 		return "rh/pessoas/index";
+	}
+	
+	@GetMapping("rh/pessoas/nova")
+	public String novaPessoa(@ModelAttribute("pessoa") Pessoa pessoa) {
+		return "rh/pessoas/form";
+		
+	}
+	
+	@PostMapping("/rh/pessoas/salvar")
+	public String salvarPessoa(@ModelAttribute("pessoa") Pessoa pessoa) {
+		pessoaRepo.save(pessoa);
+		return "redirect:/rh/pessoas";
+		
 	}
 	
 }
